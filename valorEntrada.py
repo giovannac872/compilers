9dc3b497b8138ee468a8095aea130bc92937dee9
Giovanna <giovannac872@gmail.com>
2019-10-03 13:43:58 -0300

    .

--- valorEntrada.py
+++ valorEntrada.py
@@ -2,12 +2,24 @@ import ply.lex as lex
 
 palavrasReservadas = {
         'programa' : 'PROGRAMA',
+        'car': 'CARACTERE',
+        'int': 'INTEIRO',
+        'retorne': 'RETORNE',
+        'leia': 'LEIA',
+        'escreva': 'ESCREVA',
+        'novalinha': 'NOVALINHA',
+        'se': 'SE',
+        'entao': 'ENTAO',
+        'senao': 'SENAO',
+        'enquanto': 'ENQUANTO',
+        'execute': 'EXECUTE',
+        'e': 'OPERADORLOGICOE',
+        'ou': 'OPERADORLOGICOOU'
         
 }
 
 
 tokens = [
-    'INTEIRO',
     'OPERADORSOMA',
     'OPERADORSUBTRACAO',
     'OPERADORVEZES',
@@ -16,14 +28,27 @@ tokens = [
     'FECHAPARENTESES',
     'ATRIBUICAO',
     'IDENTIFICADOR',
-    'newline',
     'error',
     'CADEIACARACTERE',
-    'CARACTERE',
     'EMPTYSPACE',
     'PONTOEVIRGULA',
     'ABRECOLCHETE',
-    'FECHACOLCHETE'
+    'FECHACOLCHETE',
+    'VIRGULA',
+    'ASPAS',
+    'COMPARACAOMAIOR',
+    'COMPARACAOMENOR',
+    'ABRECHAVES',
+    'FECHACHAVES',
+    'OPERADORTERNARIO',
+    'DOISPONTOS',
+    'COMPARACAOIGUAL',
+    'COMPARACAODIFERENTE',
+    'COMPARACAOMAIOROUIGUAL',
+    'COMPARACAOMENOROUIGUAL',
+    'OPERADORNEGACAO',
+    'COMENTARIOUMALINHA',
+    'COMENTARIOMAISUMALINHA'
 ] + list(palavrasReservadas.values())
 
 
@@ -32,12 +57,38 @@ t_OPERADORSOMA = r'\+'
 t_OPERADORSUBTRACAO = r'\-'
 t_OPERADORVEZES = r'\*'
 t_OPERADORDIVISAO = r'/'
+t_OPERADORTERNARIO = r'\?'
+t_OPERADORNEGACAO = r'!'
 t_ABREPARENTESES = r'\('
 t_FECHAPARENTESES = r'\)'
 t_ATRIBUICAO = r'\='
 t_PONTOEVIRGULA = r';'
 t_ABRECOLCHETE = r'\['
 t_FECHACOLCHETE = r'\]'
+t_VIRGULA = r','
+t_ASPAS = r'"'
+t_ABRECHAVES = r'\{'
+t_FECHACHAVES = r'\}'
+t_DOISPONTOS = r':'
+t_COMPARACAOIGUAL = r'=='
+t_COMPARACAODIFERENTE = r'!='
+t_COMPARACAOMAIOR = r'>'
+t_COMPARACAOMENOR = r'<'
+t_COMPARACAOMAIOROUIGUAL = r'>='
+t_COMPARACAOMENOROUIGUAL = r'<='
+
+
+t_PROGRAMA = r'programa'
+t_RETORNE = r'retorne'
+t_LEIA = r'leia'
+t_ESCREVA = r'escreva'
+t_SE = r'se'
+t_ENTAO = r'entao'
+t_SENAO = r'senao'
+t_ENQUANTO = r'enquanto'
+t_EXECUTE = r'execute'
+t_OPERADORLOGICOOU = r'ou'
+t_OPERADORLOGICOE = r'e'
 
 
 def t_INTEIRO(t):
@@ -45,13 +96,12 @@ def t_INTEIRO(t):
     t.value = int(t.value)
     return t
 
-def t_newline(t):
+def t_NOVALINHA(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
 #t_ignore = r' \t'
 
-t_PROGRAMA = r'programa'
 
 def t_IDENTIFICADOR(t):
     r'[a-zA-Z_][a-zA-Z_0-9]*'
@@ -68,9 +118,21 @@ def t_CADEIACARACTERE(t):
 def t_CARACTERE(t):
     r"\'.\'"
     return t
+
+def t_COMENTARIOUMALINHA(t):
+    r'//.*'
+    t.lexer.lineno += 1
+    pass 
+
+def t_COMENTARIOMAISUMALINHA(t):
+    r'/\*(.|\n)* \*/'
+    t.lexer.lineno += len(t.value)
+    pass
+
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
+
 
 def t_EMPTYSPACE(t):
     r'\s'
@@ -80,7 +142,7 @@ def t_EMPTYSPACE(t):
 
 lexer = lex.lex()
 
-lexer.input("()")
+lexer.input("/**/")
 #arquivo.close()
 #print(palavrasReservadas['programa'])
 teste = "programa"
